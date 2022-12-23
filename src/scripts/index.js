d29fb7cc9b37184cbb74c0448ba576733dbc2851
ArthurAthayde <arthur.athaydee@gmail.com>
2022-12-23 17:56:34 -0300

    pokeapiv2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,43 +1,20 @@
-async function renderizaPokemons() {
+async function renderizaPokemons(array) {
     const ulTag = document.querySelector('ul');
     ulTag.innerHTML = '';
 
-    const listaDePokemons = await consomePokeAPI()
-
-    listaDePokemons.results.forEach(pokemon => {
+    array.forEach(pokemon => {
         const cardPokemon = createCardPokemon(pokemon)
-        ulTag.appendChild(cardPokemon);
+        ulTag.append(cardPokemon);
     })
 }
 
+function createCardPokemon({ url, name }) {
 
-async function getPokeByName(pokeName) {
-    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => {
-            return response.json()
-        })
-        .then(response => {
-            renderizaPokemons(pokeName);
-            console.log(response);
-            return response.species;
-        })
-
-    return pokemon;
-}
-
-function createCardPokemon({ name, url }) {
-    const numeroNaPokedex = url.slice(34, -1);
-
-    const ulTag = document.querySelector('ul');
     const li = document.createElement('li');
     const img = document.createElement('img');
     const pokeName = document.createElement('h3');
 
+    const numeroNaPokedex = url.slice(-3, -1);
     img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroNaPokedex}.png`;
     pokeName.innerText = name;
 
@@ -51,47 +28,4 @@ function searchPokemon(pokeAtual) {
 
     const pokemon = createCardPokemon(pokeAtual);
     ulTag.appendChild(pokemon);
-
 }
-
-async function consomePokeAPIByName(name) {
-    const loading = document.querySelector('#loading')
-    const pokemonsDaAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then(response => {
-            return response.json()
-        })
-        .then(response => {
-            searchPokemon(response.species)
-            return response;
-        })
-
-    return pokemonsDaAPI
-}
-
-
-
-function renderSearch() {
-    const searchInput = document.querySelector('input');
-    const searchBtn = document.querySelector('#searchBtn');
-
-    searchBtn.addEventListener('click', async () => {
-        const pokeName = searchInput.value.toLocaleLowerCase();
-        if (searchInput.value == "") {
-            alert("Não temos um Pokemon sem nome registrado na nossa pokedex =(");
-        } else {
-            const pokeInput = await getPokeByName(pokeName);
-        }
-
-    })
-}
-
-getPokeByName('clefairy')
-renderSearch()
-consomePokeAPI()
-renderizaPokemons()
-// renderPokemon()
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -6,16 +6,45 @@ async function consomePokeAPI() {
             'Content-Type': 'application/json'
         }
     })
-        .then(response =>{
-           return response.json()
+        .then(response => {
+            return response.json()
         })
         .then(response => {
+            renderizaPokemons(response.results)
             return response;
         })
         .catch(error => console.log(error))
 
-    // loading.classList.add('hidden')
+    loading.classList.add('hidden')
+
+    return pokemonsDaAPI
+}
+async function consomePokeAPIByName(name) {
+    const pokemonsDaAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => {
+            return response.json()
+        })
+        .then(response => {
+            searchPokemon(response.species)
+            return response;
+        })
 
     return pokemonsDaAPI
 }
 
+function renderSearch() {
+    const searchInput = document.querySelector('input');
+    const searchBtn = document.querySelector('#searchBtn');
+
+    searchBtn.addEventListener('click', async () => {
+        const pokeName = searchInput.value.toLocaleLowerCase();
+        await consomePokeAPIByName(pokeName);
+    })
+}
+consomePokeAPI()
+renderSearch()
